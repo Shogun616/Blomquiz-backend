308da26f7430b96d4fd7c53964c0512af3e72c6c
SofHldn <sofie.holden@iths.se>
2021-05-07 14:09:13 +0200

    ta fram random svarsalternativ

--- database.js
+++ database.js
@@ -13,26 +13,27 @@ let db = new sqlite3.Database(DBSOURCE, (err) => {
             name TEXT NOT NULL,
             info TEXT NOT NULL,
             url TEXT NOT NULL UNIQUE,
-            level INTEGER NOT NULL 
+            level INTEGER NOT NULL,
+            questionNumber INTEGER NOT NULL 
             )`, (err) => {
             if (err) {
                 //table already created
                 console.log('table already created')
             } else {
                 //table just created, creating some rows
-                var insert = 'INSERT INTO flowers (name, info, url, level) VALUES (?,?,?,?)'
-                db.run(insert, ['Tussilago', '', 'Tussilago.png', 1])
+                var insert = 'INSERT INTO flowers (name, info, url, level, questionNumber) VALUES (?,?,?,?,?)'
+                db.run(insert, ['Tussilago', '', 'Tussilago.png', 1, 1])
 
-                db.run(insert, ['Blåsippa', '', 'Blasippa.jpg', 1])
-                db.run(insert, ['Vitsippa', '', 'vitsippa.jpg', 1])
-                db.run(insert, ['Krokus', '', 'krokus.jpg', 1])
-                db.run(insert, ['Påsklilja', '', 'pasklilja.jpg', 1])
-                db.run(insert, ['Snödroppe', '', 'snadroppe.jpg', 1])
-                db.run(insert, ['Blåstjärna', '', 'blasjarna.jpg', 1])
-                db.run(insert, ['Tulpan', '', 'tulpan.jpg', 1])
-                db.run(insert, ['Hyacint', '', 'hyacint.jpg', 1])
-                db.run(insert, ['Präskrage', '', 'prastkrage.jpg', 1])
-                db.run(insert, ['Gullviva', '', 'gullviva.jpg', 2])
+                db.run(insert, ['Blåsippa', '', 'Blasippa.jpg', 1, 2])
+                db.run(insert, ['Vitsippa', '', 'vitsippa.jpg', 1, 3])
+                db.run(insert, ['Krokus', '', 'krokus.jpg', 1, 4])
+                db.run(insert, ['Påsklilja', '', 'pasklilja.jpg', 1, 5])
+                db.run(insert, ['Snödroppe', '', 'snadroppe.jpg', 1, 6])
+                db.run(insert, ['Blåstjärna', '', 'blasjarna.jpg', 1, 7])
+                db.run(insert, ['Tulpan', '', 'tulpan.jpg', 1, 8])
+                db.run(insert, ['Hyacint', '', 'hyacint.jpg', 1, 9])
+                db.run(insert, ['Präskrage', '', 'prastkrage.jpg', 1, 10])
+                db.run(insert, ['Gullviva', '', 'gullviva.jpg', 2, 1])
 
 
 
