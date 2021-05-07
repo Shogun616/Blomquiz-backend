f7d75be84a409c15cdc7d90ab2d7e3f60105f183
andreas nehl <andenk@hotmail.se>
2021-05-07 09:01:49 +0200

    insert

--- database.js
+++ database.js
@@ -23,6 +23,22 @@ let db = new sqlite3.Database(DBSOURCE, (err) => {
                 var insert = 'INSERT INTO flowers (name, info, url, level) VALUES (?,?,?,?)'
                 db.run(insert, ['Tussilago', '', 'Tussilago.png', 1])
 
+                db.run(insert, ['Blåsippa', '', 'Blasippa.jpg', 1])
+                db.run(insert, ['Vitsippa', '', 'vitsippa.jpg', 1])
+                db.run(insert, ['Krokus', '', 'krokus.jpg', 1])
+                db.run(insert, ['Påsklilja', '', 'pasklilja.jpg', 1])
+                db.run(insert, ['Snödroppe', '', 'snadroppe.jpg', 1])
+                db.run(insert, ['Blåstjärna', '', 'blasjarna.jpg', 1])
+                db.run(insert, ['Tulpan', '', 'tulpan.jpg', 1])
+                db.run(insert, ['Hyacint', '', 'hyacint.jpg', 1])
+                db.run(insert, ['Präskrage', '', 'prastkrage.jpg', 1])
+                db.run(insert, ['Gullviva', '', 'gullviva.jpg', 1])
+
+
+
+
+
+
 
             }
         })
