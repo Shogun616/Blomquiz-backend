a1532f567c6b764219ab375192e1095b409f4047
SofHldn <sofie.holden@iths.se>
2021-05-11 09:34:44 +0200

    la till resterande blommor

--- database.js
+++ database.js
@@ -23,17 +23,26 @@ let db = new sqlite3.Database(DBSOURCE, (err) => {
                 //table just created, creating some rows
                 var insert = 'INSERT INTO flowers (name, info, url, level, questionNumber) VALUES (?,?,?,?,?)'
 
-                db.run(insert, ['Tussilago', '', 'Tussilago.png', 1, 1])
-                db.run(insert, ['Blåsippa', '', 'Blasippa.jpg', 1, 2])
+                db.run(insert, ['Tussilago', '', 'tussilago.png', 1, 1])
+                db.run(insert, ['Blåsippa', '', 'blasippa.jpg', 1, 2])
                 db.run(insert, ['Vitsippa', '', 'vitsippa.jpg', 1, 3])
                 db.run(insert, ['Krokus', '', 'krokus.jpg', 1, 4])
                 db.run(insert, ['Påsklilja', '', 'pasklilja.jpg', 1, 5])
-                db.run(insert, ['Snödroppe', '', 'snadroppe.jpg', 1, 6])
-                db.run(insert, ['Blåstjärna', '', 'blasjarna.jpg', 1, 7])
+                db.run(insert, ['Snödroppe', '', 'snodroppe.jpg', 1, 6])
+                db.run(insert, ['Blåstjärna', '', 'blastjarna.jpg', 1, 7])
                 db.run(insert, ['Tulpan', '', 'tulpan.jpg', 1, 8])
                 db.run(insert, ['Hyacint', '', 'hyacint.jpg', 1, 9])
                 db.run(insert, ['Präskrage', '', 'prastkrage.jpg', 1, 10])
                 db.run(insert, ['Gullviva', '', 'gullviva.jpg', 2, 1])
+                db.run(insert, ['Förgätmigej', '', 'forgatmigej.jpg', 2, 2])
+                db.run(insert, ['Midsommarblomster', '', 'midsommarblomster.jpg', 2, 3])
+                db.run(insert, ['Johannesört', '', 'johannesort.jpg', 2, 4])
+                db.run(insert, ['Fingerborgsblomma', '', 'fingerborgsblomma.jpg', 2, 5])
+                db.run(insert, ['Blåeld', '', 'blaeld.jpg', 2, 6])
+                db.run(insert, ['Smörblomma', '', 'smorblomma.jpg', 2, 7])
+                db.run(insert, ['Vallmo', '', 'vallmo.jpg', 2, 8])
+                db.run(insert, ['Blåklocka', '', 'blaklocka.jpg', 2, 9])
+                db.run(insert, ['Rödklöver', '', 'rodklover.jpg', 2, 10])
 
 
 
