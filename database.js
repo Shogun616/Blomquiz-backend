9dfd9f8dba8160019ef305a0b31c064916d39ba1
Joel Lindqvist <joellindqvist@iths.se>
2021-05-26 14:44:20 +0200

    level 3 påbörjat

--- database.js
+++ database.js
@@ -12,20 +12,24 @@ let db = new sqlite3.Database(DBSOURCE, (err) => {
             id INTEGER PRIMARY KEY,
             name TEXT NOT NULL,
             info TEXT NOT NULL,
-            url TEXT NOT NULL UNIQUE,
+            url TEXT NOT NULL,
             level INTEGER NOT NULL,
-            questionNumber INTEGER NOT NULL 
+            questionNumber INTEGER NOT NULL, 
+            infoQuestion TEXT,
+            answerA TEXT,
+            answerB TEXT,
+            answerC TEXT
             )`, (err) => {
             if (err) {
                 //table already created
                 console.log('table already created')
             } else {
                 //table just created, creating some rows
-                var insert = 'INSERT INTO flowers (name, info, url, level, questionNumber) VALUES (?,?,?,?,?)'
+                var insert = 'INSERT INTO flowers (name, info, url, level, questionNumber, infoQuestion, answerA, answerB, answerC) VALUES (?,?,?,?,?,?,?,?,?)'
 
-                db.run(insert, ['Tussilago', 'En vårblomma. Kallas även Hästhov Blommar vanligen mellan april-maj, ibland så tidigt som i februari. Vanligast i södra och mellersta Sverige. Växer på fuktig, grusig eller lerig mark. En viktig växt för våra bin.', 'tussilago.png', 1, 1])
-                db.run(insert, ['Blåsippa', 'Blåsippan är en fridlyst blomma vilket innebär att det är förbjudet att gräva eller plocka blomman samt använda den till försäljning. Blommar mellan april-maj. Vanligast i södra Sverige. Blomman växer mest i lövskogar. Är en ört och kan bli hundraårig.', 'blasippa.jpg', 1, 2])
-                db.run(insert, ['Vitsippa', 'Vitsippan är en ört som blommar mellan april-maj. De vita blommorna kan skifta i rött när de är nyutslagna samt går mot sitt slut. Vitsippan finns i nästan hela landet och växer i skogs- och hagmarker.', 'vitsippa.jpg', 1, 3])
+                db.run(insert, ['Tussilago', 'Tussilago är en vårblomma. Kallas även Hästhov Blommar vanligen mellan april-maj, ibland så tidigt som i februari. Vanligast i södra och mellersta Sverige. Växer på fuktig, grusig eller lerig mark. En viktig växt för våra bin.', 'tussilago.png', 1, 1, 'Vad kallas tussilago även för?', 'Hästhov', 'Maskros', 'Smörblomma'])
+                db.run(insert, ['Blåsippa', 'Blåsippan är en fridlyst blomma vilket innebär att det är förbjudet att gräva eller plocka blomman samt använda den till försäljning. Blommar mellan april-maj. Vanligast i södra Sverige. Blomman växer mest i lövskogar. Är en ört och kan bli hundraårig.', 'blasippa.jpg', 1, 2, ' Varför är det förbjudet att gräva,plocka och/eller sälja blåsippan?', 'Giftig','Fridlyst','Explosivt'])
+                db.run(insert, ['Vitsippa', 'Vitsippan är en ört som blommar mellan april-maj. De vita blommorna kan skifta i rött när de är nyutslagna samt går mot sitt slut. Vitsippan finns i nästan hela landet och växer i skogs- och hagmarker.', 'vitsippa.jpg', 1, 3, 'Vilken färg blir vitsippan när de är nyutslagen eller går mot sitt slut?', 'Rosa', 'Blå', 'Röd'])
                 db.run(insert, ['Krokus', 'Vårkrokus är en lökväxt och tillhör familjen irisväxter. Ursprung i Sydeuropa och Sydosteuropa. Krokusen blommar mellan mars-april, ibland ännu tidigare. Den blommar i lila, gula och vita nyanser.', 'krokus.jpg', 1, 4])
                 db.run(insert, ['Påsklilja', 'Påskliljan är en flerårig lökväxt som tillhör familjen Amaryllisväxter. Blomman har en gul tub- eller trumpetliknande bikrona. Blommar april-maj.', 'pasklilja.jpg', 1, 5])
                 db.run(insert, ['Snödroppe', 'Snödroppen tillhör familjen Amaryllis och är en av de mest kända inom släktet. Blommar mellan februari-april och finns i hela Sverige. Snödroppen har vita kronblad och är en lökväxt. Löken och växten är giftig.', 'snodroppe.jpg', 1, 6])
@@ -43,9 +47,6 @@ let db = new sqlite3.Database(DBSOURCE, (err) => {
                 db.run(insert, ['Vallmo', 'Vallmosläktet är ett växtsläkte med omkring 120 arter i familjen. Var förr mycket vanlig på åkrar där man odlade korn. Numera ser man den oftast på åkerrenar och vägkanter. Vallmosläktet odlades redan 5 000 år före Kristus i Mesopotamien och de har t om påträffats i egyptiska gravar.', 'vallmo.jpg', 2, 8])
                 db.run(insert, ['Blåklocka', 'Blåklocka är en ganska späd, flerårig ört, den växer ofta i glesa tuvor med veka uppstigande stjälkar och blir bara ett par decimeter hög. Blåklocka och/eller ängsklocka är Dalarnas landskapsblomma.\n', 'blaklocka.jpg', 2, 9])
                 db.run(insert, ['Rödklöver', 'Rödklöver blommar hela sommaren och fortsätter att blomma till långt in på hösten. Rödklöver är vanlig i hela landet och den odlas också ofta som vallväxt. Den växer vanligen på kulturpåverkad mark.', 'rodklover.jpg', 2, 10])
-
-
-
 
 
 
